6734f74d26e3ef838893743361dc6044978b8dc6
rakati <ouhaddounouredine@gmail.com>
2024-06-04 03:06:21 +0100

    fix code style

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-    let amenities = {};
+  const amenities = {};
 
-    // Listen for changes on each input checkbox tag
-    $('input[type="checkbox"]').change(function () {
-        const amenityId = $(this).data('id');
-        const amenityName = $(this).data('name');
+  // Listen for changes on each input checkbox tag
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
 
-        if ($(this).is(':checked')) {
-            amenities[amenityId] = amenityName;
-        } else {
-            delete amenities[amenityId];
-        }
+    if ($(this).is(':checked')) {
+      amenities[amenityId] = amenityName;
+    } else {
+      delete amenities[amenityId];
+    }
 
-        // Update the h4 tag inside the div Amenities with the list of Amenities checked
-        const amenityList = Object.values(amenities).join(', ');
-        $('div.amenities h4').text(amenityList);
-    });
+    // Update the h4 tag inside the div Amenities with the list of Amenities checked
+    const amenityList = Object.values(amenities).join(', ');
+    $('div.amenities h4').text(amenityList);
+  });
 });
