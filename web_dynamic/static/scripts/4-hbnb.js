2ee2e7b78cff217fa696468a4175f03f42e3b350
amine-zzr <aminezoukri@gmail.com>
2024-06-06 15:53:18 +0000

    include the functionality of making a new POST request to places_search when the button is clicked

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success' && data.status === 'OK') {
+      $('#api_status').attr('class', 'available');
+    } else {
+      $('#api_status').attr('class', '');
+    }
+  });
+
+  const amenities = {};
+
+  // Listen for changes on each input checkbox tag
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      amenities[amenityId] = amenityName;
+    } else {
+      delete amenities[amenityId];
+    }
+
+    // Update the h4 tag inside the div Amenities with the list of Amenities checked
+    const amenityList = Object.values(amenities).join(', ');
+    $('div.amenities h4').text(amenityList);
+  });
+
+  // Load places when the button is clicked
+  $('button').click(function () {
+    $('.places').empty(); // Clear previous results
+
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ amenities: Object.keys(amenities) }),
+      success: function (data) {
+        data.forEach(place => {
+          $('.places').append(
+            `<article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">$${place.price_by_night}</div>
+              </div>
+              <div class="information">
+                <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+                <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+                <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+              </div>
+              <div class="description">
+                ${place.description}
+              </div>
+            </article>`
+          );
+        });
+      }
+    });
+  });
+});
